dfe78e835ea30d5d9be7164a5c0846ab798a59b4
Kundan Kumar Mourya <32189172+kundankumarmourya@users.noreply.github.com>
2018-09-29 14:17:52 +0530

    Adding Js

---
+++ script.js
@@ -0,0 +1,11 @@
+
+$('body').on('mouseenter mouseleave', '.dropdown', function (e) {
+    var dropdown = $(e.target).closest('.dropdown');
+    var menu = $('.dropdown-menu', dropdown);
+    dropdown.addClass('show');
+    menu.addClass('show');
+    setTimeout(function () {
+        dropdown[dropdown.is(':hover') ? 'addClass' : 'removeClass']('show');
+        menu[dropdown.is(':hover') ? 'addClass' : 'removeClass']('show');
+    }, 300);
+});
